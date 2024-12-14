d61b9656bdb0265979722d3a14d0f577cecbdd45
YasbelAgr <yasbelnac06@gmail.com>
2024-12-13 21:54:31 -0600

    first commit

---
+++ calculadora de descuento.r
@@ -0,0 +1,5 @@
+
+precio_original = float(input("Introduce el precio original"))
+descuento = float(input("Introduce el porcentaje de descuento"))
+precio_final = precio_original * (1 - descuento / 100)
+print(f"El precio final despues del descuento es: "{precio_final})
